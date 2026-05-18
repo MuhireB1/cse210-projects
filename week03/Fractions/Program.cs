55fc7e2f7d242c81c4dd1be8cf3ba606313608cc
MuhireB1 <muhirebienvenue@gmail.com>
2026-05-18 12:34:47 +0200

    Included other instances

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -12,10 +12,18 @@ class Program
         // Console.WriteLine(Fraction1.GetBottom());
 
         // Second Instance of Fraction
-        Fraction Fraction2 = new Fraction(1, 3);
+        Fraction Fraction2 = new Fraction(5, 1);
         Console.WriteLine(Fraction2.GetFractionString());
         Console.WriteLine(Fraction2.GetDecimalValue());
 
+        // Third Instance of Fraction
+        Fraction Fraction3 = new Fraction(3, 4);
+        Console.WriteLine(Fraction3.GetFractionString());
+        Console.WriteLine(Fraction3.GetDecimalValue());
 
+        // Fourth Instance of Fractio
+        Fraction Fraction4 = new Fraction(1, 3);
+        Console.WriteLine(Fraction4.GetFractionString());
+        Console.WriteLine(Fraction4.GetDecimalValue());
     }
 }
